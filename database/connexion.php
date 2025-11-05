897c018043f132bc9ee21f81a7db5341e56862a7
SoufainNajmi <soufainnajmi@gmail.com>
2025-11-05 12:19:38 +0100

    database

---
+++ database/connexion.php
@@ -0,0 +1,11 @@
+<?php
+// 1️⃣ Connexion à la base de données
+$host = "localhost";       // serveur
+$user = "root";            // nom d'utilisateur MySQL
+$password = "";            // mot de passe MySQL
+$BD = "marchi_bordo";    // nom de votre base de données
+
+$conn = mysqli_connect($host, $user, $password, $BD);
+
+
+?>
